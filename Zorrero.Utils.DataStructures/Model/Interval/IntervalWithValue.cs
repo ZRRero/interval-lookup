7c52c192f1d4b6cc2bd435d6a3d56cfe92afbb31
David Useche <david.useche@masivian.com>
2021-02-11 22:42:43 -0500

    Updated IntervalWithValue.cs for overlapped intervals compareTo

--- Zorrero.Utils.DataStructures/Model/Interval/IntervalWithValue.cs
+++ Zorrero.Utils.DataStructures/Model/Interval/IntervalWithValue.cs
@@ -21,8 +21,8 @@ namespace Zorrero.Utils.DataStructures.Model.Interval
         public int CompareTo(IntervalWithValue<T, TK> other)
         {
             if (Init.CompareTo(other.Init) == 0 && End.CompareTo(other.End) == 0) return 0;
-            if (Init.CompareTo(other.End) > 0) return 1;
-            if (End.CompareTo(other.Init) < 0) return -1;
+            if (Init.CompareTo(other.Init) > 0) return 1;
+            if (End.CompareTo(other.End) < 0) return -1;
             return 0;
         }
 
