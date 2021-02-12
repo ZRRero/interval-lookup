e50f8a7d097c6c15055b468196d439b34bc03e77
David Useche <david-use@hotmail.com>
2021-02-11 22:56:58 -0500

    Init tests

---
+++ Zorrero.Utils.DataStructures.Tests/IntervalTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Zorrero.Utils.DataStructures.Tests
+{
+    public class IntervalTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
