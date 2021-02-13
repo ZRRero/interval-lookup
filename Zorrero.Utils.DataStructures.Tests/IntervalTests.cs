fc870ba5285f3cf0d447cbf3e65c29906e00b0a2
David Useche <david-use@hotmail.com>
2021-02-12 19:04:00 -0500

    Completed IntervalTests.cs

--- Zorrero.Utils.DataStructures.Tests/IntervalTests.cs
+++ Zorrero.Utils.DataStructures.Tests/IntervalTests.cs
@@ -7,6 +7,7 @@ namespace Zorrero.Utils.DataStructures.Tests
 {
     public class IntervalTests
     {
+        //Interval creation
         [Fact]
         public void ShouldCreateIntervalCorrectly()
         {
@@ -22,6 +23,7 @@ namespace Zorrero.Utils.DataStructures.Tests
             Assert.Throws<InvalidIntervalException>(() => new IntervalWithValue<long, long>(2,1,0));
         }
 
+        //Interval compare
         [Fact]
         public void ShouldEvaluateAsEqualIntervals()
         {
@@ -84,6 +86,96 @@ namespace Zorrero.Utils.DataStructures.Tests
             var intervalOne = new IntervalWithValue<long, long>(1,5,0);
             var intervalTwo = new IntervalWithValue<long, long>(-5,10,0);
             Assert.Equal(1, intervalOne.CompareTo(intervalTwo));
+        }
+
+        //Interval evaluate
+        [Fact]
+        public void GivenIntervalShouldEvaluateUnderIncludingInit()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.UNDER, interval.Evaluate(0, true, false));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateUnderNotIncludingInit()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.UNDER, interval.Evaluate(1, false, false));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateUpperIncludingEnd()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.UPPER, interval.Evaluate(6, false, true));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateUpperNotIncludingEnd()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.UPPER, interval.Evaluate(5, false, false));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateContainedIncludingInit()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.CONTAINED, interval.Evaluate(1, true, false));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateContainedNotIncludingInit()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.CONTAINED, interval.Evaluate(2, false, false));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateContainedIncludingEnd()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.CONTAINED, interval.Evaluate(5, false, true));
+        }
+        
+        [Fact]
+        public void GivenIntervalShouldEvaluateContainedNotIncludingEnd()
+        {
+            var interval = new IntervalWithValue<long, long>(1,5,0);
+            Assert.Equal(IntervalResult.CONTAINED, interval.Evaluate(4, false, false));
+        }
+        
+        //Interval Equals
+        [Fact]
+        public void TwoIntervalWithAllDifferentPropertiesShouldNotBeEqual()
+        {
+            var intervalOne = new IntervalWithValue<long, long>(1,5,0);
+            var intervalTwo = new IntervalWithValue<long, long>(-5,10,-9);
+            Assert.False(intervalOne.Equals(intervalTwo));
+        }
+
+        [Fact]
+        public void TwoIntervalWithSameValueAndDifferentInitAndEndShouldNotBeEqual()
+        {
+            var intervalOne = new IntervalWithValue<long, long>(1,5,0);
+            var intervalTwo = new IntervalWithValue<long, long>(-5,10,0);
+            Assert.False(intervalOne.Equals(intervalTwo));
+        }
+        
+        [Fact]
+        public void TwoIntervalWithSameInitAndEndAndDifferentValueShouldNotBeEqual()
+        {
+            var intervalOne = new IntervalWithValue<long, long>(1,5,0);
+            var intervalTwo = new IntervalWithValue<long, long>(1,5,1);
+            Assert.False(intervalOne.Equals(intervalTwo));
+        }
+        
+        [Fact]
+        public void TwoIntervalWithSamePropertiesShouldBeEqual()
+        {
+            var intervalOne = new IntervalWithValue<long, long>(1,5,0);
+            var intervalTwo = new IntervalWithValue<long, long>(1,5,0);
+            Assert.True(intervalOne.Equals(intervalTwo));
         }
     }
 }
